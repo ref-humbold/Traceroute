d3f3e9f3dc5a41acedb423c4a75bc5a076698044
R. Kaleta <rafkaleta@onet.eu>
2025-07-21 00:27:08 +0200

    Simple tests for IP address class

---
+++ test/Ip4Address_test.cpp
@@ -0,0 +1,44 @@
+#include <gtest/gtest.h>
+#include "Ip4Address.hpp"
+
+TEST(Ip4AddressTest, constructor_WhenNumber_ThenCreated)
+{
+    // when
+    Ip4Address result(0x7F000001);
+
+    // then
+    EXPECT_EQ(Ip4Address("127.0.0.1"), result);
+}
+
+TEST(Ip4AddressTest, constructor_WhenValidString_ThenCreated)
+{
+    // when
+    Ip4Address result("127.0.0.1");
+
+    // then
+    EXPECT_EQ(Ip4Address("127.0.0.1"), result);
+}
+
+TEST(Ip4AddressTest, operatorUint32T_ThenIPv4Number)
+{
+    // given
+    Ip4Address address("127.0.0.1");
+
+    // when
+    uint32_t result = static_cast<uint32_t>(address);
+
+    // then
+    EXPECT_EQ(0x7F000001, result);
+}
+
+TEST(Ip4AddressTest, operatorString_ThenIPv4String)
+{
+    // given
+    Ip4Address address("127.0.0.1");
+
+    // when
+    std::string result = static_cast<std::string>(address);
+
+    // then
+    EXPECT_EQ("127.0.0.1", result);
+}
