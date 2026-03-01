96c7e1351df43d7362f479a1ec0e45913521ba40
R. Kaleta <rafkaleta@onet.eu>
2026-03-01 11:39:59 +0100

    Parameterized IP address tests

--- test/Ip4Address_test.cpp
+++ test/Ip4Address_test.cpp
@@ -1,44 +1,63 @@
 #include <gtest/gtest.h>
 #include "Ip4Address.hpp"
 
-TEST(Ip4AddressTest, constructor_WhenNumber_ThenCreated)
+class Ip4AddressTest_Pairs : public testing::TestWithParam<std::pair<std::string, uint32_t>>
 {
-    // when
-    Ip4Address result(0x7F000001);
+};
 
-    // then
-    EXPECT_EQ(Ip4Address(0x7F000001), result);
-}
+INSTANTIATE_TEST_SUITE_P(,
+        Ip4AddressTest_Pairs,
+        testing::Values(std::make_pair("127.0.0.1", 0x7F000001),
+                std::make_pair("8.8.8.8", 0x08080808),
+                std::make_pair("172.16.0.255", 0xAC1000FF),
+                std::make_pair("10.44.144.244", 0x0A2C90F4)));
 
-TEST(Ip4AddressTest, constructor_WhenValidString_ThenCreated)
-{
-    // when
-    Ip4Address result("127.0.0.1");
-
-    // then
-    EXPECT_EQ(Ip4Address(0x7F000001), result);
-}
-
-TEST(Ip4AddressTest, operatorUint32T_ThenIPv4Number)
+TEST_P(Ip4AddressTest_Pairs, constructor_WhenNumber_ThenCreated)
 {
     // given
-    Ip4Address address(0x7F000001);
+    std::pair<std::string, uint32_t> params = GetParam();
 
     // when
-    uint32_t result = static_cast<uint32_t>(address);
+    Ip4Address result(params.second);
 
     // then
-    EXPECT_EQ(0x7F000001, result);
+    EXPECT_EQ(Ip4Address(params.first), result);
 }
 
-TEST(Ip4AddressTest, operatorString_ThenIPv4String)
+TEST_P(Ip4AddressTest_Pairs, constructor_WhenValidString_ThenCreated)
 {
     // given
-    Ip4Address address(0x7F000001);
+    std::pair<std::string, uint32_t> params = GetParam();
 
     // when
-    std::string result = static_cast<std::string>(address);
+    Ip4Address result(params.first);
 
     // then
-    EXPECT_EQ("127.0.0.1", result);
+    EXPECT_EQ(Ip4Address(params.second), result);
+}
+
+TEST_P(Ip4AddressTest_Pairs, operatorUint32T_ThenIPv4Number)
+{
+    // given
+    std::pair<std::string, uint32_t> params = GetParam();
+    Ip4Address address(params.first);
+
+    // when
+    auto result = static_cast<uint32_t>(address);
+
+    // then
+    EXPECT_EQ(params.second, result);
+}
+
+TEST_P(Ip4AddressTest_Pairs, operatorString_ThenIPv4String)
+{
+    // given
+    std::pair<std::string, uint32_t> params = GetParam();
+    Ip4Address address(params.second);
+
+    // when
+    auto result = static_cast<std::string>(address);
+
+    // then
+    EXPECT_EQ(params.first, result);
 }
